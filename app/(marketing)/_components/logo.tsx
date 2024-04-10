4804ab2f7472b1c56b19bfd4b0f95fe16daa16e9
Vivek <me.vivekgupta9@gmail.com>
2024-04-10 10:12:39 +0530

    auth completed

--- app/(marketing)/_components/logo.tsx
+++ app/(marketing)/_components/logo.tsx
@@ -11,8 +11,21 @@ const font = Poppins({
 export const Logo = () => {
   return (
     <div className=" hidden md:flex items-center gap-x-2">
-      <Image src={"/logo.svg"} height="40" width="40" alt="Logo" />
-      <p className={cn('font-semibold', font.className)}>Jotion</p>
+      <Image
+        src={"/logo.svg"}
+        className="dark:hidden"
+        height="40"
+        width="40"
+        alt="Logo"
+      />
+      <Image
+        src={"/logo-dark.svg"}
+        className="hidden dark:block"
+        height="40"
+        width="40"
+        alt="Logo"
+      />
+      <p className={cn("font-semibold", font.className)}>Jotion</p>
     </div>
   )
 }
---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://amusing-hedgehog-9.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+}
